8cfc38fcec8e7df2ae50a308965672e1f451af11
Edward Kmett <ekmett@gmail.com>
2015-05-12 19:32:52 -0400

    Added the monad attached to a multicategory. section headers. added variants. lobotomized foldrArgs

--- Multicategories.hs
+++ Multicategories.hs
@@ -3,6 +3,8 @@ module Multicategories where
 
 import Control.Applicative
 import Control.Category
+import Control.Comonad
+import Control.Monad (ap)
 import Control.Monad.ST
 import Data.Constraint
 import Data.Functor.Identity
@@ -31,6 +33,14 @@ rmap :: (forall a. f a -> g a) -> Rec f as -> Rec g as
 rmap _ RNil = RNil
 rmap f (a :& as) = f a :& rmap f as
 
+data Variant :: (k -> *) -> [k] -> * where
+  Head :: f a -> Variant f (a ': as)
+  Tail :: Variant f as -> Variant f (a ': as)
+
+--------------------------------------------------------------------------------
+-- * Graded
+--------------------------------------------------------------------------------
+
 class Graded f where
   grade :: f is o -> Rec Proxy is
 
@@ -43,25 +53,35 @@ instance KnownGrade '[] where
 instance KnownGrade is => KnownGrade (i ': is) where
   gradeVal = Proxy :& gradeVal
 
+--------------------------------------------------------------------------------
+-- * Arguments for a multicategory form a polycategory
+--------------------------------------------------------------------------------
+
 data Args :: ([k] -> k -> *) -> [k] -> [k] -> * where
   Nil  :: Args f '[] '[]
   (:-) :: f i o -> Args f is os -> Args f (i ++ is) (o ': os)
 
 infixr 5 :-
 
-foldrArgs :: (forall i o is os. f i o -> r is os -> r (i ++ is) (o ': os)) -> r '[] '[] -> Args f m n -> r m n
+foldrArgs :: (forall i o is. f i o -> r is -> r (i ++ is)) -> r '[] -> Args f m n -> r m
 foldrArgs _ z Nil = z
 foldrArgs f z (a :- as) = f a (foldrArgs f z as)
 
-data Grade is os = Grade { degrade :: Rec Proxy is }
-
 gradeArgs :: Graded f => Args f is os -> Rec Proxy is
-gradeArgs as = degrade $ foldrArgs (\a (Grade r) -> Grade $ grade a `rappend` r) (Grade RNil) as
+gradeArgs = foldrArgs (\a r -> grade a `rappend` r) RNil
+
+--------------------------------------------------------------------------------
+-- * Multicategories
+--------------------------------------------------------------------------------
 
 -- | multicategory / planar colored operad
 class Graded f => Multicategory f where
   ident   :: f '[a] a
   compose :: f bs c -> Args f as bs -> f as c
+
+--------------------------------------------------------------------------------
+-- * Symmetric Multicategories
+--------------------------------------------------------------------------------
 
 -- | generators for the symmetric groupoid Sigma_k
 data Swap :: [a] -> [a] -> * where
@@ -71,7 +91,11 @@ data Swap :: [a] -> [a] -> * where
 class Multicategory f => Symmetric f where
   swap :: Swap as bs -> f as o -> f bs o
 
--- | The endomorphism multicategory over a Hask
+--------------------------------------------------------------------------------
+-- * Endo
+--------------------------------------------------------------------------------
+
+-- | The endomorphism multicategory over a Hask; the multicategory represented by Hask.
 data Endo is o where
   Endo :: !(Rec Proxy is) -> (Rec Identity is -> o) -> Endo is o
 
@@ -91,6 +115,10 @@ instance Multicategory Endo where
       (l,r) -> Identity (g l) :& go gs r
     go Nil RNil = RNil
 
+--------------------------------------------------------------------------------
+-- * Free multicategory
+--------------------------------------------------------------------------------
+
 -- | free multicategory given graded atoms
 data Free :: ([k] -> k -> *) -> [k] -> k -> * where
   Ident :: Free f '[a] a
@@ -109,18 +137,66 @@ instance Graded f => Multicategory (Free f) where
 
 -- each operad is a contravariant functor in (Args f) in its first argument, and covariant in (Op f) in its second
 
--- polykinded Const
-newtype K a b = K a
+--------------------------------------------------------------------------------
+-- * Kleisli arrows of outrageous fortune
+--------------------------------------------------------------------------------
 
--- the monad associated with an operad
+data Atkey a i j where
+  Atkey :: a -> Atkey a i i
+ 
+--------------------------------------------------------------------------------
+-- * (Co)monads from a planar operad
+--------------------------------------------------------------------------------
+
+-- The monad associated with an operad. This generalizes the notion of the writer monad to an arbitrary operad
 data M (f :: [()] -> () -> *) (a :: *) where
-  M :: f is '() -> Rec (K a) is -> M f a
+  M :: f is '() -> Rec (Atkey a '()) is -> M f a
 
--- instance Multicategory f => Monad (M f)
+instance Functor (M f) where
+  fmap f (M s d) = M s (rmap (\(Atkey a) -> Atkey (f a)) d)
+
+instance Multicategory f => Applicative (M f) where
+  pure = return
+  (<*>) = ap
+
+instance Multicategory f => Monad (M f) where
+  return a = M ident (Atkey a :& RNil)
+  (>>=) = bind where
+    bind :: forall a b. M f a -> (a -> M f b) -> M f b
+    bind (M s0 d0) f = go d0 $ \ as ds -> M (compose s0 as) ds where
+      go :: Rec (Atkey a '()) is -> (forall os. Args f os is -> Rec (Atkey b '()) os -> r) -> r
+      go RNil k = k Nil RNil
+      go (Atkey a :& is) k = go is $ \fs as -> case f a of
+        M s bs -> k (s :- fs) (rappend bs as)
+
+-- The comonad associated with an operad
+newtype W (f :: [()] -> () -> *) (a :: *) = W { runW :: forall is. f is '() -> Rec (Atkey a '()) is } -- Coatkey?
+
+instance Functor (W f) where
+  fmap f (W g) = W (rmap (\(Atkey a) -> Atkey (f a)) . g)
+
+instance Multicategory f => Comonad (W f) where
+  extract (W f) = case f ident of
+    Atkey a :& RNil -> a
+
+--------------------------------------------------------------------------------
+-- * Indexed (Co)monads from a Multicategory
+--------------------------------------------------------------------------------
 
 -- a mcbride-style indexed monad associated with a multicategory
 data IM (f :: [k] -> k -> *) (a :: k -> *) (o :: k) where
   IM :: f is o -> Rec a is -> IM f a o
+
+-- instance Multicategory f => IMonad (IM f)
+
+-- an indexed comonad associated with a multicategory
+newtype IW (f :: [k] -> k -> *) (a :: k -> *) (o :: k) = IW { runIW :: forall is. f is o -> Rec a is }
+
+-- instance Multicategory f => IComonad (IW f)
+
+--------------------------------------------------------------------------------
+-- * A category obtained by keeping only 1-argument multimorphisms
+--------------------------------------------------------------------------------
 
 -- | One category you get when given an operad. This is a forgetful functor that forgets all but the unary arrows.
 newtype Oper f a b = Oper { runOper :: f '[a] b }
@@ -131,6 +207,10 @@ opermap f (Oper a) = Oper (f a)
 instance Multicategory f => Category (Oper f) where
   id = Oper ident
   Oper f . Oper g = Oper $ compose f (g :- Nil)
+
+--------------------------------------------------------------------------------
+-- * Free multicategory from a category
+--------------------------------------------------------------------------------
 
 -- | Build a free multicategory from a category, left adjoint to Oper
 data C :: (i -> i -> *) -> [i] -> i -> * where
